390edae76f82857d59a73b1cdf4faae113499e82
Cem C. Tutum <cem.celal@gmail.com>
2024-05-24 00:38:50 -0500

    updated likelihood function

--- likelihood.py
+++ likelihood.py
@@ -1,42 +1,50 @@
 import numpy as np
+from scipy.spatial.distance import cdist
 
-def likelihood(X, y, x)
+
+def likelihood(t, X, y):
+    """
+    Calculates the negative of the concentrated ln-likelihood.
+
+    Inputs:
+        t - vector of log(theta) parameters
+        X - n x k matrix of sample locations
+        y - n x 1 vector of observed data
+
+    Outputs:
+        NegLnLike - concentrated log-likelihood *-1 for minimising
+        Psi - correlation matrix
+        U - Choleski factorisation of correlation matrix
+    """
     
-    global ModelInfo
-    X=ModelInfo.X;
-    y=ModelInfo.y;
-    theta=10.^x;
-    p=2;  % added p definition (February 10)
-    n=size(X,1);
-    one=ones(n,1);
-    
-    # Pre-allocate memory
-    Psi=zeros(n,n);
-    # Build upper half of correlation matrix
-    for i=1:n
-    	for j=i+1:n
-    		Psi(i,j)=exp(-sum(theta.*abs(X(i,:)-X(j,:)).^p))
-    
-    # Add upper and lower halves and diagonal of ones plus 
-    # small number to reduce ill-conditioning
-    Psi = Psi + Psi' + eye(n) + eye(n).*eps; 
+    theta = 10.0**t
+    p = 2
+    n, k = X.shape
+    one = np.ones(n)
+
+    # Build correlation matrix
+    dist = cdist(X, X, metric='minkowski', p=p)
+    Psi = np.exp(-np.sum(theta * np.abs(dist), axis=2))
+
+    # Add diagonal and small number to reduce ill-conditioning
+    Psi = Psi + np.eye(n) + np.eye(n) * np.finfo(float).eps
 
     # Cholesky factorisation
-    [U,p]=chol(Psi);
-    
-    % Use penalty if ill-conditioned
-    if p>0
-        NegLnLike=1e4;
-    else
-        
-        % Sum lns of diagonal to find ln(abs(det(Psi)))
-        LnDetPsi=2*sum(log(abs(diag(U))));
-    
-        % Use back-substitution of Cholesky instead of inverse
-        mu=(one'*(U\(U'\y)))/(one'*(U\(U'\one)));
-        SigmaSqr=((y-one*mu)'*(U\(U'\(y-one*mu))))/n;
-        NegLnLike=-1*(-(n/2)*log(SigmaSqr) - 0.5*LnDetPsi);
-    end
+    try:
+        U = np.linalg.cholesky(Psi)
+    except np.linalg.LinAlgError:
+        NegLnLike = 1e4
+        Psi = None
+        U = None
+        return NegLnLike, Psi, U
+
+    # Sum lns of diagonal to find ln(abs(det(Psi)))
+    LnDetPsi = 2 * np.sum(np.log(np.abs(np.diag(U))))
+
+    # Use back-substitution of Cholesky instead of inverse
+    mu = (one @ np.linalg.solve(U, np.linalg.solve(U.T, y))) / \
+         (one @ np.linalg.solve(U, np.linalg.solve(U.T, one)))
+    SigmaSqr = ((y - one * mu) @ np.linalg.solve(U, np.linalg.solve(U.T, y - one * mu))) / n
+    NegLnLike = -(-(n / 2) * np.log(SigmaSqr) - 0.5 * LnDetPsi)
+
     return NegLnLike, Psi, U
-
-
